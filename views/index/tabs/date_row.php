0ab21c88265cfba19d83fb2c786e0abef9e57729
Felix Hahn <info@hahn-felix.de>
2025-05-13 10:07:02 +0200

    Update date_row.php

--- views/index/tabs/date_row.php
+++ views/index/tabs/date_row.php
@@ -23,10 +23,10 @@ use yii\web\View;
 
 <div class="date-row" id="date-row-<?= $index ?>">
     <div class="row">
-        <div class="col-xs-2 col-lg-1 row-icons">
+        <div class="col-2 col-lg-1 row-icons">
             <?= Icon::get('calendar-check-o')->size(Icon::SIZE_LG) ?>
         </div>
-        <div class="col-xs-5 col-lg-6">
+        <div class="col-5 col-lg-6">
             <?= $form->field($model, "[$index]day")
                 ->widget(DatePicker::class, [
                     'options' => [
@@ -35,7 +35,7 @@ use yii\web\View;
                 ])
                 ->label(false) ?>
         </div>
-        <div class="col-xs-5 text-end">
+        <div class="col-5 text-end">
             <?= Button::asLink()->danger()
                 ->icon('fa-times')
                 ->action('removeDateRow')
@@ -53,17 +53,16 @@ use yii\web\View;
                 ->action('addDateRow', $contentContainer->createUrl('/lets-meet/index/add-date-row', ['hash' => TabsStateManager::instance()->hash]))
                 ->options([
                     'title' => Yii::t('LetsMeetModule.base', 'Delete'),
-                    'class' => 'add-row',
-                    'style' => ['display' => $last ? '' : 'none']
+                    'class' => 'add-row' . $last ? '' : ' d-none',
                 ])
             ?>
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-2 col-lg-1 row-icons">
+        <div class="col-2 col-lg-1 row-icons">
             <?= Icon::get('clock-o')->size(Icon::SIZE_LG) ?>
         </div>
-        <div class="col-xs-10 col-lg-11">
+        <div class="col-10 col-lg-11">
             <?= $form->field($model, "[$index]times")
                 ->widget(TimeSlotPicker::class, [
                     'options' => [
