7b3d2e6dd33238b582878e5d9f53c352ee304e88
HumHub Translations <hello@humhub.com>
2025-10-02 22:38:01 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/notification.php
+++ messages/de/notification.php
@@ -4,7 +4,7 @@ return [
   'All invited participants for the <strong>{meeting}</strong> Let’s Meet in the Space <strong>{space}</strong> have voted.' => '',
   'All invited participants have voted.' => '',
   'Description' => 'Beschreibung',
-  'Let\'s Meet' => '',
+  'Let\'s Meet' => 'Let\'s Meet',
   'Organizer' => 'Organisator',
   'Receive Notifications when someone invites you to a vote.' => '',
   'View Online' => 'Online ansehen',
